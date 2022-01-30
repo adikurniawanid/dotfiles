1db9915c033de80b32e87b4c294b5f0d7d7b9cfb
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-01-30 10:45:08 +0700

    feat: fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+#starship
+starship init fish | source
+set -U fish_greeting ""
+figlet -f slant "Rhapsody"
+
+#alias
+alias ll="ls -l"
+alias la="ls -al"
+alias l="ls"
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias vi="nvim"
+alias vim="nvim"
+alias xampp='cd /opt/lampp/ && sudo ./manag*' 
+alias gotop='~/gotop' 
