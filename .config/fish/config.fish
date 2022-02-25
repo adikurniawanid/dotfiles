c49e11b5384c5e5de21d217964f5903e516ec18b
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-02-25 08:05:09 +0700

    minor(fish): add tide prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set -U fish_greeting ""
-figlet -f slant "Rhapsody"
+#figlet -f slant "> Rhapsody"
+figlet "> Rhapsody"
+
 
 #alias
 alias ll="ls -l"
@@ -9,5 +11,7 @@ alias cp="cp -iv"
 alias mv="mv -iv"
 alias vi="nvim"
 alias vim="nvim"
-alias xampp='cd /opt/lampp/ && sudo ./manag*' 
-alias gotop='~/gotop' 
+alias xampp='cd /opt/lampp/ && sudo ./manag*'
+alias gotop='~/App/gotop' 
+alias hugo='~/App/hugo' 
+
